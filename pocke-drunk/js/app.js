4020813c6ce5e36bf15818fb0d4d47eaf8e57904
calciferDeLioncourt <jimsamzara@gmail.com>
2020-08-17 22:40:28 -0500

    sw

--- pocke-drunk/js/app.js
+++ pocke-drunk/js/app.js
@@ -1,3 +1,50 @@
 if(navigator.serviceWorker){
     navigator.serviceWorker.register('sw.js');
 };
+
+// if(window.caches){
+
+//     //crear cache
+//     caches.open('prueba-1');
+
+//     caches.open('prueba-2');
+
+//     // //confirmar cache
+//     // caches.has('prueba-2').then( console.log );
+
+//     // //borrar cache
+//     // caches.delete('prueba-1').then( console.log );
+
+//     caches.open('cache-v1.1').then( cache => {
+
+//         //agrega un arcrchivo al cache
+//         // cache.add ('index.html');
+
+//         //agraga varios archivos (arreglo)
+
+//         cache.addAll([
+//             'index.html',
+//             'css/style.css',
+//             'js/pocke-drunk.js',
+//             'json/api-borrachos.json'
+//         ]).then( () => {
+
+//             // cache.delete('profiles/')
+
+//             cache.put( 'index.html', new Response('Hola mundo') );
+
+//         });
+
+//         // cache.match('css/style.css')
+//         //     .then( res => {
+
+//         //         res.text().then( console.log );
+
+//         //     });
+
+//         caches.keys().then( keys => {
+//             console.log(keys);
+//         });
+//     });
+
+// };
--- pocke-drunk/sw.js
+++ pocke-drunk/sw.js
@@ -1,19 +1,78 @@
-self.addEventListener('fetch', event=> {
+const CACHE_STATIC_NAME = 'static-v1',
+    CACHE_DYNAMIC_NAME = 'dynamic-v1',
+    CACHE_INMUTABLE_NAME = 'inmutable-v1',
+    CACHE_DYNAMIC_LIMIT = 50;
+
+
+function limpiarCache( cacheName, numeroItems ){
+
+    caches.open( cacheName )
     
-    const offlineResp = new Response(`
+        .then( cache =>  {
+
+            return cache.keys()
+                .then( keys => {
+                    // console.log(keys);
+
+                    if( keys.length > numeroItems){
+                        cache.delete( keys[0] )
+                            .then( limpiarCache( cacheName, numeroItems ) );
+                    };
+
+                });
+
+        });
+
+};
+
+
+self.addEventListener('install', e => {
+
+    const cacheProm = caches.open( CACHE_STATIC_NAME )
+        .then( cache => {
+
+            return cache.addAll([
+                'index.html',
+                'css/style.css',
+                'js/app.js',
+                'json/api-borrachos.json'
+
+            ]);
+
+        });
+
+
+    e.waitUntil( cacheProm );
+
+});
+
+self.addEventListener('fetch', e => {
     
-        Bienvenido a mi Página web
-
-        Disculpa, pero para usarla, necesitas internet
+    // >>>>> -->>>>> ----- 2.cache con networ fallback ----- <<<<<-- <<<<<
     
-    `);
+    const respuesta = caches.match( e.request)
+        .then( res => {
+            if( res ) return res;
+
+            //no existe archivo
+            //ir a web
+            console.log( 'No existe', e.request.url );
+
+            return fetch( e.request ).then( newResp => {
+
+                //agragar archivo faltante
+                caches.open( CACHE_DYNAMIC_NAME )
+                    .then( cache => {
+                        cache.put( e.request, newResp );
+                        limpiarCache( CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT );
+                    });
 
 
-    const resp = fetch(event.request)
-                    .catch( () => offlineResp);
+                return newResp.clone();
+            });
+            
+        })
 
-    event.respondWith( resp );
-    
 
-
+    e.respondWith(respuesta)
 });
